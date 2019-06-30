3cf221fda27205b710a95c0a32eab062c4474226
Viswa Kodela <viswakodela@gmail.com>
2019-06-30 17:02:41 -0400

    Initial Commit

---
+++ UIVIewPropertyAnimator Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIVIewPropertyAnimator Example
+//
+//  Created by Viswa Kodela on 6/30/19.
+//  Copyright © 2019 Viswa Kodela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
